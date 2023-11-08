2f9aeec014e378f3b341e496b6e201ab30e70f5f
mariesaeter <marie.saeter@gmail.com>
2023-11-08 10:32:26 +0100

    Added footer background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
-    <footer className=" text-fantasy-blue bg-sky-blue-dark py-10 mt-5 lg:mt-8 flex justify-center">
-      <div className="max-w-sm lg:max-w-lg text-xs text-center">
+    <footer className=" text-fantasy-blue pt-5 mt-5 lg:mt-8 flex place-items-center justify-center bg-[url('../assets/footer-bg.svg')] bg-cover min-h-[10rem]">
+      <div className="max-w-sm lg:max-w-lg text-xs text-center ">
         <p>Copyright © 2023 Holidaze, fictional adventures </p>
         <p>
           Design and development by Marie Sæter project exam 2 at Noroff School
