f944639a6ea757fb9933e8742e75b91f3506ad95
mariesaeter <marie.saeter@gmail.com>
2023-11-16 18:42:15 +0100

    Fetched profile successfully

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom";
 import { NavBgGradient } from "../components/styled-components/Navbg";
-import { useGetProfile } from "../hooks/api/useGetProfile";
+import { useApiAuth } from "../hooks/api/useGetProfile";
 import { URL_PROFILE } from "../constants/url";
+
 // // import { AuthContext } from "../context/AuthContext";
 // import { loadLocal } from "../utils/localStorage";
 
@@ -9,11 +10,11 @@ export const Profile = () => {
   let params = useParams();
 
   // const profileUser = loadLocal("profile");
-  const { userProfile, isLoading, isError } = useGetProfile(
+  const { data, isLoading, isError } = useApiAuth(
     `${URL_PROFILE}/${params.name}`
   );
 
-  console.log(userProfile);
+  console.log(data);
   if (isLoading) {
     return <div>Is loading</div>;
   }
@@ -27,7 +28,7 @@ export const Profile = () => {
       <div className="mx-5 my-5 md:my-10 md:mx-16">
         <h1 className="mb-5">Profile</h1>
         <div>hello</div>
-        {/* <img src={userProfile.avatar} alt={`${userProfile.name} avatar`} /> */}
+        <img src={data.avatar} alt={`${data.name} avatar`} />
       </div>
     </>
   );
