164827cc9ec21ae0a33328f532779e3b493ae9c3
mariesaeter <marie.saeter@gmail.com>
2023-11-17 11:54:52 +0100

    Makes it possible to change avatar

---
+++ src/components/forms/AvatarForm.jsx
@@ -0,0 +1,60 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useEffect } from "react";
+import { iconClass, inputClass } from "../../constants";
+import { BtnPrimary } from "../styled-components/Buttons";
+import { apiPut } from "../../hooks/api/useApiPut";
+import { URL_PROFILE } from "../../constants/url";
+import { useParams } from "react-router-dom";
+
+const AvatarSchema = yup.object({
+  avatar: yup.string().url(),
+});
+
+export const AvatarForm = (oldAvatar) => {
+  let params = useParams();
+
+  const url = `${URL_PROFILE}/${params.name}`;
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm({ resolver: yupResolver(AvatarSchema) });
+
+  useEffect(() => {
+    reset();
+  }, [isSubmitSuccessful, reset]);
+
+  const useOnSubmit = async (data) => {
+    try {
+      console.log(data);
+      await apiPut(data, `${url}/media`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form
+      id="avatarForm"
+      onSubmit={handleSubmit(useOnSubmit)}
+      className=" mx-auto bg-body-white w-full p-2 rounded-lg"
+    >
+      <label htmlFor="avatar">Avatar</label>
+      <div className=" block mx-auto relative text-earth-brown h-11 mb-2.5">
+        <i className={iconClass("fa-image")}></i>
+        <input
+          type="url"
+          name="avatar"
+          {...register("avatar")}
+          className={inputClass}
+          placeholder={oldAvatar.oldAvatar}
+        />
+        <p className="text-error-dark text-sm mt-1">{errors.avatar?.message}</p>
+      </div>
+      <BtnPrimary btnText="Save avatar" type="submit" />
+    </form>
+  );
+};
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import { NavBgGradient } from "../components/styled-components/Navbg";
 import { useApiAuth } from "../hooks/api/useGetProfile";
 import { URL_PROFILE } from "../constants/url";
+import { Modal } from "../components/Modal";
+import { AvatarForm } from "../components/forms/AvatarForm";
 
 // // import { AuthContext } from "../context/AuthContext";
 // import { loadLocal } from "../utils/localStorage";
@@ -9,10 +11,9 @@ import { URL_PROFILE } from "../constants/url";
 export const Profile = () => {
   let params = useParams();
 
+  const url = `${URL_PROFILE}/${params.name}`;
   // const profileUser = loadLocal("profile");
-  const { data, isLoading, isError } = useApiAuth(
-    `${URL_PROFILE}/${params.name}`
-  );
+  const { data, isLoading, isError } = useApiAuth(url);
 
   console.log(data);
   if (isLoading) {
@@ -26,9 +27,13 @@ export const Profile = () => {
     <>
       <NavBgGradient />
       <div className="mx-5 my-5 md:my-10 md:mx-16">
-        <h1 className="mb-5">Profile</h1>
+        <h1 className="mb-5">My profile</h1>
         <div>hello</div>
         <img src={data.avatar} alt={`${data.name} avatar`} />
+        <Modal modalLinkText="Change avatar">
+          <h2>Change avatar</h2>
+          <AvatarForm oldAvatar={data.avatar} />
+        </Modal>
       </div>
     </>
   );
