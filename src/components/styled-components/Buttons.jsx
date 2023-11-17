1029c92c1b47437f97c54c1cebc65706d53f9a95
mariesaeter <marie.saeter@gmail.com>
2023-11-17 14:14:01 +0100

    Styled profile page

--- src/components/styled-components/Buttons.jsx
+++ src/components/styled-components/Buttons.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export const WideButton = ({ classes, btnText, type, onClick }) => {
   return (
     <button
@@ -39,6 +41,36 @@ export const BtnSecondary = ({ btnText, type, onClick }) => {
       btnText={btnText}
       type={type}
       onClick={onClick}
+    />
+  );
+};
+
+const WideLink = ({ location, classes, text }) => {
+  return (
+    <Link
+      to={location}
+      className={`w-full h-10 tracking-wider rounded-lg transition ease-in-out duration-500  mb-2.5 ${classes} `}
+    >
+      {text}
+    </Link>
+  );
+};
+
+export const LinkPrimary = ({ location, text }) => {
+  return (
+    <WideLink
+      classes="bg-fantasy-blue hover:bg-fantasy-blue-hover text-body-white"
+      text={text}
+      location={location}
+    />
+  );
+};
+export const LinkSecondary = ({ location, text }) => {
+  return (
+    <WideLink
+      classes="text-earth-brown-dark border-earth-brown hover:border-earth-brown-dark border-2"
+      text={text}
+      location={location}
     />
   );
 };
